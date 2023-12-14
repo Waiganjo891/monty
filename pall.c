3cc9746be778dd34f73439d4d8bac136c16dc1d7
Waiganjo <waiganothiongo12@gmail.com>
2023-12-14 01:11:46 -0800

    Task

--- pall.c
+++ pall.c
@@ -2,7 +2,6 @@
 /**
  * pall - prints all values on the stack
  * @stack: pointer to the head of the stack
- * @line_number: line number in the file
  */
 void pall(stack_t **stack)
 {
@@ -13,4 +12,22 @@ void pall(stack_t **stack)
 		printf("%d\n", temp->n);
 		temp = temp->next;
 	}
+}
+
+/**
+ * push_opcode - A void
+ * @stack: A stack_s
+ * @line: A character
+ * @line_number: An integer
+ */
+void push_opcode(stack_s **stack, char *line, int line_number)
+{
+	int data = atoi(line);
+
+	if (data == 0 && strcmp(line, "0") != 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	push(stack, data);
 }
