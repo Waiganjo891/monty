98322e585318ad1043fe4b83df38bdda25a8046a
Waiganjo <waiganothiongo12@gmail.com>
2024-01-10 01:02:56 -0800

    Tasks

--- main.c
+++ main.c
@@ -9,18 +9,7 @@ int main(int argc, char *argv[])
 {
 	FILE *file;
 	stack_t *stack = NULL;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	instruction_t a[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{NULL, NULL}
-	};
 	unsigned int line_number = 0;
-	char *opcode;
-	int i;
 
 	if (argc != 2)
 	{
@@ -33,34 +22,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		line_number++;
-		opcode = strtok(line, " \t\n");
-		if (opcode)
-		{
-			i = 0;
-			while (a[i].opcode)
-			{
-				if (strcmp(opcode, a[i].opcode) == 0)
-				{
-					a[i].f(&stack, line_number);
-					break;
-				}
-				i++;
-			}
-			if (!a[i].opcode)
-			{
-				fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-				fclose(file);
-				free(line);
-				free_stack(stack);
-				return (EXIT_FAILURE);
-			}
-		}
-	}
+	process_instructions(&stack, &line_number);
 	fclose(file);
-	free(line);
 	free_stack(stack);
 	return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <errno.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -41,5 +40,6 @@ void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
+void process_instructions(stack_t **stack, unsigned int *line_number);
 
 #endif
